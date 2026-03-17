1e7de2174091678094bb31d6b4a2d8c9a41aff97
Mohamed <mido.basiouny123@gmail.com>
2026-03-17 23:33:54 +0200

    Initialize project structure

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Bookly());
+}
+
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
