c2c254e12df808e81afc9259f05fbe5582d548a6
Mohamed <mido.basiouny123@gmail.com>
2026-03-18 02:53:12 +0200

    creating NewestItem

--- lib/features/home/presentation/views/home_view_widgets/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/home_view_widgets/widgets/home_view_body.dart
@@ -2,9 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:test2/core/utils/styles.dart';
-import 'book_image.dart';
 import 'custom_sliver_app_bar.dart';
 import 'featured_books_list_view.dart';
+import 'newest_item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -14,7 +14,21 @@ class HomeViewBody extends StatelessWidget {
     return CustomScrollView(
       slivers: [
         CustomSliverAppBar(),
+        SliverToBoxAdapter(child: Gap(20)),
         SliverToBoxAdapter(child: FeaturedBooksListView()),
+        SliverToBoxAdapter(
+          child: Gap(MediaQuery.of(context).size.height * 0.055),
+        ),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const .only(bottom: 25, left: 16),
+            child: Text(
+              'Newest List',
+              style: Styles.style20.copyWith(fontWeight: .bold),
+            ),
+          ),
+        ),
+        SliverToBoxAdapter(child: NewestItem()),
       ],
     );
   }
---
+++ lib/features/home/presentation/views/home_view_widgets/widgets/newest_item.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:test2/core/utils/styles.dart';
+
+import 'book_image.dart';
+
+class NewestItem extends StatelessWidget {
+  const NewestItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: .only(left: 16, right: 16),
+      height: 105 * 1.2,
+      decoration: BoxDecoration(
+        color: Colors.white.withAlpha(39),
+        borderRadius: .circular(16),
+      ),
+      child: Row(
+        children: [
+          BookImage(),
+          Gap(16),
+          Expanded(
+            child: Column(
+              children: [
+                Container(
+                  margin: .only(top: 4),
+                  width: MediaQuery.of(context).size.width * 0.7,
+                  child: Text(
+                    'Harry Potter and the Goblet of Fire',
+                    overflow: .ellipsis,
+                    maxLines: 2,
+                    style: GoogleFonts.inknutAntiqua().copyWith(fontSize: 20),
+                  ),
+                ),
+                Gap(4),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.7,
+                  child: Text(
+                    'J.K. Rowling',
+                    overflow: .ellipsis,
+                    maxLines: 1,
+                    style: Styles.style14.copyWith(color: Colors.amber),
+                  ),
+                ),
+
+                Spacer(),
+
+                Row(
+                  children: [
+                    Icon(Icons.star_rounded, color: Colors.amber),
+                    Gap(4),
+                    Text(
+                      '4.8',
+                      style: Styles.style16.copyWith(color: Colors.amber),
+                    ),
+                    Spacer(),
+                  ],
+                ),
+                Gap(4),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
