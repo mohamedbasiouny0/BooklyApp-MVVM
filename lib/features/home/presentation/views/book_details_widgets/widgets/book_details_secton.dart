530b8c88f29ff20a2c08800f13d117787cd29964
Mohamed <mido.basiouny123@gmail.com>
2026-03-18 05:09:23 +0200

    refactor BookDetailsSection

--- lib/features/home/presentation/views/book_details_widgets/widgets/book_details_secton.dart
+++ lib/features/home/presentation/views/book_details_widgets/widgets/book_details_secton.dart
@@ -18,32 +18,40 @@ class _BookDetailsSectonState extends State<BookDetailsSecton> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(
-          width: MediaQuery.of(context).size.width * 0.48,
-          child: BookImage(),
-        ),
-        Gap(20),
-        Text(
-          textAlign: .center,
-          'Harry Potter and the Goblet of Fire',
-          style: GoogleFonts.inknutAntiqua(fontSize: 25),
-        ),
-        Gap(5),
-        Text(
-          textAlign: .center,
-          'J.K. Rowling',
-          style: Styles.style18.copyWith(color: Colors.amber),
-        ),
-        Gap(5),
-        BookRating(mainAxisAlignment: .center),
-        Gap(40),
-        Padding(
-          padding: const .symmetric(horizontal: 16),
-          child: Column(
+    return Padding(
+      padding: const .symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.49,
+            child: BookImage(),
+          ),
+          Gap(20),
+          Text(
+            textAlign: .center,
+            'Harry Potter and the Goblet of Fire',
+            style: GoogleFonts.inknutAntiqua(fontSize: 25),
+          ),
+          Gap(5),
+          Text(
+            textAlign: .center,
+            'J.K. Rowling',
+            style: Styles.style18.copyWith(color: Colors.amber),
+          ),
+          Gap(5),
+          BookRating(mainAxisAlignment: .center),
+          Column(
             crossAxisAlignment: .stretch,
             children: [
+              Text(
+                'Publish Date: 1988',
+                style: Styles.style16.copyWith(color: Colors.grey.shade400),
+              ),
+              Text(
+                'Pages count: 288',
+                style: Styles.style16.copyWith(color: Colors.grey.shade400),
+              ),
+              Gap(20),
               Text(
                 'Describtion:',
                 style: Styles.style20.copyWith(fontWeight: .bold),
@@ -54,7 +62,7 @@ class _BookDetailsSectonState extends State<BookDetailsSecton> {
                   setState(() {});
                 },
                 child: Text(
-                  'data',
+                  'data bsjkbdsabjkasadsjkjhkdsahjkdshdshakadsfhjkdsfhjkdfhjksfdshjkfhjkdhjkdfshjkfdshjkfhjafshjasfhsafhldjsfhjkladjhafsdhjlksfajhasfkdlkfhjadslfhjdsklhdsfjalkfhjdsklahjdsflkhjdsflhsfjdlaksfadhjklafdhjklfadshjkldfhjkldfshjkldfshjklfsdahjklafhjklsdfjhkfshjksahjklfhjkdsfilweanicfwoefynewfhuewifhiruegfiulcranlflgcarhjkgfarngkfqnaegrujflgerjakfngbjrakngfbcrakgyufjergyukhbjfkewgyuhfgyuewhfyhjbnfhyubksdhjkcndshuijklnfuirshgfyuihjrwguihjknwrjiof',
                   style: Styles.style14,
                   maxLines: tapped ? 400 : 4,
                   overflow: .ellipsis,
@@ -62,8 +70,8 @@ class _BookDetailsSectonState extends State<BookDetailsSecton> {
               ),
             ],
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
--- lib/features/home/presentation/views/book_details_widgets/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/book_details_widgets/widgets/book_details_view_body.dart
@@ -1,11 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:test2/core/utils/styles.dart';
 import 'package:test2/features/home/presentation/views/book_details_widgets/widgets/book_details_secton.dart';
+import 'package:test2/features/home/presentation/views/home_view_widgets/widgets/book_image.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: BookDetailsSecton());
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          BookDetailsSecton(),
+          Gap(40),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Align(
+              alignment: .centerLeft,
+              child: Text(
+                'You can also like',
+                style: Styles.style20.copyWith(fontWeight: .bold),
+              ),
+            ),
+          ),
+          Gap(20),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.15,
+            child: ListView.builder(
+              scrollDirection: .horizontal,
+              itemCount: 10,
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding: .symmetric(horizontal: 8),
+                  child: BookImage(),
+                );
+              },
+            ),
+          ),
+          Gap(30),
+        ],
+      ),
+    );
   }
 }
