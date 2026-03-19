c77bda81b544a2bafdd0805c81488a785b1757f3
Mohamed <mido.basiouny123@gmail.com>
2026-03-19 22:56:19 +0200

    refactoring : deleting SearchIconButton

--- lib/features/search/presentation/view/widgets/custom_text_field.dart
+++ lib/features/search/presentation/view/widgets/custom_text_field.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: use_build_context_synchronously
 
+import 'dart:collection';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -14,44 +16,34 @@ class CustomTextField extends StatefulWidget {
 }
 
 class _CustomTextFieldState extends State<CustomTextField> {
-  late final TextEditingController _controller;
   final PublishSubject<String> subject = PublishSubject<String>();
 
   @override
   void initState() {
     super.initState();
-    _controller = TextEditingController();
     subject.debounceTime(Duration(milliseconds: 600)).distinct().listen((
-      qurey,
+      String q,
     ) {
-      if (qurey.trim().isNotEmpty) {
-        context.read<SearchBooksCubit>();
-      } else {}
+      if (q.trim().isNotEmpty) {
+        context.read<SearchBooksCubit>().getSearchBooks(q: q);
+      }
     });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return TextField(
-      controller: _controller,
       cursorColor: Colors.white,
       style: TextStyle(color: Colors.white, fontSize: 20),
       onChanged: (value) {
         subject.add(value);
       },
       decoration: InputDecoration(
-        suffix: IconButton(
-          onPressed: () {
-            subject.add(_controller.text);
-          },
-          icon: Icon(CupertinoIcons.search, color: Colors.grey.shade400),
+        suffix: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Icon(CupertinoIcons.search, color: Colors.grey.shade400),
         ),
+
         contentPadding: .only(left: 8),
         hintText: 'Search',
         hintStyle: TextStyle(color: Colors.grey.shade400, fontSize: 19),
